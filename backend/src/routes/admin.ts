3a0aa61dcf8fba2d34672beb4302ddfcbf56acf9
Muhammad Alkali Muawiya <123987902+MM-Alkali@users.noreply.github.com>
2023-08-18 07:43:07 +0100

    Reset Password Update

--- backend/src/routes/admin.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { AdminLogin} from '../controllers/authController'
-const router = express.Router();
-
-router.post("/login", AdminLogin);
-
-export default router;
--- backend/src/routes/superadmin.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { SuperAdminLogin,createAdmin } from '../controllers/authController'
-import {Auth} from '../middlewares/auth'
-const router = express.Router();
-
-router.post("/login",SuperAdminLogin);
-router.post("/create-admin",Auth,createAdmin)
-
-export default router;
