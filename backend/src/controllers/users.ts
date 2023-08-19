65e9ccb1cf10643fd0c17d25d42fac21aef7cdb7
Muhammad Alkali Muawiya <123987902+MM-Alkali@users.noreply.github.com>
2023-08-19 10:08:00 +0100

    Request Management

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -132,7 +132,7 @@ export const createUser = async (req: Request, res: Response) => {
       await SendActivationLink(
         email,
         `${firstName} ${lastName}`,
-        `http://localhost:3000/users/update/${newUser._id}`,
+        savedUser.id,
       );
     }
 
