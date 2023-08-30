84ada9783df6d5a8706705678b06e09fd20dd312
Emmanuel Omeje <emmanuelomeje@meristemng.local>
2023-08-30 08:22:19 +0100

    Updating branch

---
+++ src/controllers/passwords.ts
@@ -0,0 +1,179 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import {
+  GenerateOtp,
+  GeneratePassword,
+  GenerateSalt,
+  GenerateToken,
+  SendPasswordResetOTP,
+} from '../utils/notifications';
+import { User } from '../models/users';
+import { Verification } from '../models/passwords';
+
+const STATUS_ACTIVE = 'active';
+
+const sendErrorResponse = (
+  res: Response,
+  statusCode: number,
+  message: string,
+) => {
+  return res.status(statusCode).json({ message });
+};
+
+// Change Password API
+export const changePassword = async (req: Request, res: Response) => {
+  try {
+    const { currentPassword, newPassword, confirmPassword } = req.body;
+    const userId = req.params.id;
+
+    const user = await User.findOne({ _id: userId });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      return sendErrorResponse(res, 401, 'Invalid current password');
+    }
+
+    if (newPassword !== confirmPassword) {
+      return sendErrorResponse(res, 400, 'Passwords do not match');
+    }
+
+    const salt = await GenerateSalt();
+    const hashedNewPassword = await GeneratePassword(newPassword, salt);
+
+    user.status = STATUS_ACTIVE;
+    await user.save();
+    await user.updateOne({ password: hashedNewPassword });
+
+    return res.status(200).json({
+      staus: 200,
+      message: 'Password changed successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse(res, 500, 'Internal server error');
+  }
+};
+
+export const forgotPassword = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return sendErrorResponse(
+        res,
+        404,
+        'User not found, kindly register first',
+      );
+    }
+
+    const verification = await Verification.findOne({ userId: user.id });
+
+    const { otp, expiry } = await GenerateOtp();
+    const token = await GenerateToken(email, '', '', '', '', res);
+    await SendPasswordResetOTP(email, otp);
+
+    if (verification) {
+      verification.otp = otp;
+      verification.expiry = expiry;
+      await verification.save();
+    } else {
+      await Verification.create({
+        otp,
+        expiry,
+        userId: user.id,
+      });
+    }
+
+    return res.status(200).json({ message: 'OTP sent successfully', token });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse(res, 500, 'Internal server error');
+  }
+};
+
+export const verifyOTP = async (req: Request | any, res: Response) => {
+  try {
+    const { otp } = req.body;
+
+    const { email } = req.user;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(403).json({ error: 'Invalid user or OTP' });
+    }
+
+    const verification = await Verification.findOne({ userId: user.id });
+
+    if (!verification || otp !== verification.otp) {
+      return res.status(401).json({ error: 'The OTP entered is not correct' });
+    }
+
+    if (verification.expiry && new Date() >= verification.expiry) {
+      return res.status(400).json({ error: 'Invalid/Expired OTP' });
+    }
+
+    await Verification.deleteOne({ userId: user.id });
+
+    return res.status(200).json({
+      message: 'OTP verified successfully. Proceed to change your password',
+    });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse(res, 500, 'Internal server error');
+  }
+};
+
+export const resetPassword = async (req: Request | any, res: Response) => {
+  const { newPassword, confirmPassword } = req.body;
+  const reg = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]+$/g;
+  const checker = reg.test(newPassword);
+
+  try {
+    const { email } = req.user;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    if (newPassword === '' || confirmPassword === '') {
+      return res.status(400).json({ error: 'password input field required' });
+    }
+    if (newPassword.length < 8 || confirmPassword.length < 8) {
+      return res
+        .status(400)
+        .json({ error: 'password must be eight characters or more' });
+    }
+    if (checker === false) {
+      return res.status(400).json({
+        error:
+          'Password should contain at least one upperCase, one special character and a number',
+      });
+    }
+
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords do not match' });
+    }
+
+    const salt = await GenerateSalt();
+    const hashedPassword = await GeneratePassword(newPassword, salt);
+
+    await user.updateOne({ password: hashedPassword });
+
+    return res.status(200).json({
+      message: 'Password changed successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    return sendErrorResponse(res, 500, 'Internal server error');
+  }
+};
---
+++ src/controllers/users.ts
@@ -0,0 +1,353 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import emailValidator from 'email-validator';
+import {
+  GenerateSalt,
+  GenerateToken,
+  GeneratePassword,
+  SendActivationLink,
+} from '../utils/notifications';
+import {
+  registerValidator,
+  loginValidator,
+  variables,
+} from '../utils/utilities';
+import { User, UserDocument } from '../models/users';
+import {
+  defaultpassword,
+  superadminemail,
+  superadminpassword,
+} from '../config';
+
+// Login
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const formValidation = loginValidator.validate(req.body, variables);
+
+    if (formValidation.error) {
+      return res
+        .status(400)
+        .json({ Error: formValidation.error.details[0].message });
+    }
+
+    // If user is SuperAdmin
+    const superadmin = [
+      {
+        email: superadminemail,
+        password: superadminpassword,
+        name: 'Super Admin',
+      },
+    ];
+
+    const admin = superadmin.find(
+      (admin) => admin.email === email && admin.password === password,
+    );
+
+    if (admin) {
+      const token = await GenerateToken(email, '', '', '', '', res); // Pass null for firstName and lastName
+      return res
+        .status(200)
+        .json({ message: 'SuperAdmin logged in Successfully', token });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    if (existingUser.status !== 'active') {
+      if (existingUser.role === 'admin') {
+        // Update the admin user's status to "active"
+        existingUser.status = 'active';
+        await existingUser.save();
+      } else {
+        return res.status(403).json({
+          error: 'Your account is deactivated, kindly contact your admin',
+        });
+      }
+    }
+
+    const passwordMatch = await bcrypt.compare(password, existingUser.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Incorrect password' });
+    }
+
+    const token = await GenerateToken(
+      email,
+      existingUser.firstName,
+      existingUser.lastName,
+      existingUser.id,
+      existingUser.role,
+      res,
+    ); // Pass firstName and lastName
+    return res.status(200).json({
+      message: 'Logged in successfully',
+      token,
+      role: existingUser.role,
+      existingUser,
+    });
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// export const login = async (req: Request, res: Response) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const token = await GenerateToken(email, res);
+
+//     const formValidation = loginValidator.validate(req.body, variables);
+
+//     if (formValidation.error) {
+//       return res
+//         .status(400)
+//         .json({ Error: formValidation.error.details[0].message });
+//     }
+
+//     // If user is SuperAdmin
+//     const superadmin = [
+//       {
+//         email: superadminemail,
+//         password: superadminpassword,
+//         name: 'Super Admin',
+//       },
+//     ];
+
+//     const admin = superadmin.find(
+//       (admin) => admin.email === email && admin.password === password,
+//     );
+
+//     if (admin) {
+//       return res
+//         .status(200)
+//         .json({ message: 'Super Admin logged in Successfully', token });
+//     }
+
+//     const existingUser = await User.findOne({ email });
+
+//     if (!existingUser) {
+//       return res.status(400).json({ error: 'User not found' });
+//     }
+//     if (existingUser.status !== 'active') {
+//       return res.status(403).json({
+//         error: 'Your account is deactivated, kindly contact your admin',
+//       });
+//     }
+
+//     const passwordMatch = await bcrypt.compare(password, existingUser.password);
+//     if (!passwordMatch) {
+//       return res.status(401).json({ error: 'Incorrect password' });
+//     }
+
+//     return res.status(200).json({
+//       message: 'Logged in successfully',
+//       token,
+//     });
+//   } catch (error) {
+//     console.error('Error logging in user:', error);
+//     return res.status(500).json({ error: 'Internal server error' });
+//   }
+// };
+
+// Create a user
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { users } = req.body;
+
+    if (!users || !Array.isArray(users)) {
+      return res.status(400).json({
+        error:
+          'Invalid input format. Please provide an object with a "users" property containing an array of users.',
+      });
+    }
+
+    const savedUsers: UserDocument[] = [];
+
+    for (const userData of users) {
+      const { firstName, lastName, email, department, role } = userData;
+
+      const formValidation = registerValidator.validate(userData, variables);
+
+      if (formValidation.error) {
+        return res
+          .status(400)
+          .json({ Error: formValidation.error.details[0].message });
+      }
+
+      if (!emailValidator.validate(email)) {
+        return res.status(400).json({ error: 'Invalid email address' });
+      }
+
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res
+          .status(400)
+          .json({ error: `User with email ${email} already exists` });
+      }
+
+      const salt = await GenerateSalt();
+      const hashedPassword = await GeneratePassword(defaultpassword, salt);
+
+      const newUser: UserDocument = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        department,
+        role,
+        status: 'inactive',
+      });
+
+      const savedUser = await newUser.save();
+      savedUsers.push(savedUser);
+
+      await SendActivationLink(
+        email,
+        `${firstName} ${lastName}`,
+        savedUser._id,
+      );
+    }
+
+    res.status(201).json({ message: 'User created successfully', savedUsers });
+  } catch (error) {
+    console.error('Error creating users:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+//Update user properties
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const updateUser = async (req: Request | any, res: Response) => {
+  try {
+    const userId = req.params.id;
+    const { firstName, lastName, email, department, role } = req.body;
+
+    const userToUpdate = await User.findById(userId);
+    if (!userToUpdate) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    // Check if email is being changed and it's not a duplicate
+    if (userToUpdate.email !== email) {
+      const existingUserWithEmail = await User.findOne({ email });
+      if (existingUserWithEmail) {
+        return res
+          .status(400)
+          .json({ error: `The email address ${email} is already in use.` });
+      }
+    }
+
+    // Restrict certain updates for admin users
+    if (
+      userToUpdate.role === 'admin' &&
+      req.user.role === 'admin' &&
+      req.user.id !== userId
+    ) {
+      return res
+        .status(403)
+        .json({ error: 'Unauthorized to update other admin information.' });
+    }
+
+    userToUpdate.firstName = firstName;
+    userToUpdate.lastName = lastName;
+    userToUpdate.email = email;
+    userToUpdate.department = department;
+    userToUpdate.role = role;
+
+    await userToUpdate.save();
+
+    return res
+      .status(200)
+      .json({ message: 'User updated successfully', userToUpdate });
+  } catch (error) {
+    console.error('Error updating user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Activate and Deactivate a user
+export const toggleActivation = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user: UserDocument | null = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Determine the new status based on the current status
+    const newStatus = user.status === 'active' ? 'inactive' : 'active';
+    user.status = newStatus;
+
+    // Save the updated user to the database
+    const updatedUser = await user.save();
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error('Error toggling user activation:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Delete a user
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.id;
+
+    const userToDelete = await User.findById(userId);
+    if (!userToDelete) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    await User.deleteOne({ _id: userId });
+
+    return res
+      .status(200)
+      .json({ message: 'User deleted successfully', userToDelete });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+// Fetch all users
+export const fetchAllUsers = async (req: Request, res: Response) => {
+  try {
+    const allUsers = await User.find();
+
+    res.json(allUsers);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const fetchSingleUser = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ id });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const logout = async (req: Request, res: Response) => {
+  res.clearCookie('token');
+  res.status(200).json({
+    message: 'Logged out successfully',
+  });
+};
