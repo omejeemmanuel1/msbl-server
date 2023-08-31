aaba363ef8e9fb0ee53639bc009d693c18d9b68d
Emmanuel Omeje <emmanuelomeje@meristemng.local>
2023-08-31 11:27:58 +0100

    Deployment

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(connectionstring, {});
     console.log('MongoDB connected successfully');
+    console.log(process.env.MONGODB_URI);
   } catch (err) {
     console.error(`MongoDB connection error: ${err}`);
     process.exit(1);
